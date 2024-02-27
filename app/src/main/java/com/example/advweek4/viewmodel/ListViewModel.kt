c04f869c800514aebccb04f8def5400a1ce21965
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-27 20:30:23 +0700

    debugging ImageView

--- app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
@@ -16,21 +16,21 @@ class ListViewModel : ViewModel() {
                 "Nonie",
                 "1998/03/28",
                 "5718444778",
-                "http://dummyimage.com/75x100" + ".jpg/cc0000/ffffff"
+                "https://i.pinimg.com/originals/62/8c/01/628c01e4b8321396e45d812a871ffd26.jpg"
             ),
             Student(
                 "13312",
                 "Rich",
                 "1994/12/14",
                 "3925444073",
-                "http://dummyimage.com/75x100" + ".jpg/5fa2dd/ffffff"
+                "https://i.pinimg.com/originals/62/8c/01/628c01e4b8321396e45d812a871ffd26.jpg"
             ),
             Student(
                 "11204",
                 "Dinny",
                 "1994/10/07",
                 "6827808747",
-                "http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1"
+                "https://i.pinimg.com/originals/62/8c/01/628c01e4b8321396e45d812a871ffd26.jpg"
             )
         )
         studentLoadErrorLD.value = false

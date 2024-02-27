ec12cead1b08374da653ff29b7e515562a5a792a
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-27 19:17:58 +0700

    Creating Model

---
+++ app/src/main/java/com/example/advweek4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.advweek4.model
+
+data class Student(
+    val id: String?,
+    val name: String?,
+    val dob: String?, // date of birth
+    val phone: String?,
+    val photoUrl: String?,
+)

c6796fec4c31eb341ccfe04f0f4bc2e55e842254
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-28 14:52:15 +0700

    Clear json, queue add, & clear the queue

--- app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/advweek4/viewmodel/ListViewModel.kt
@@ -22,29 +22,29 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
     private var queue:RequestQueue? = null
 
     fun refresh() {
-        studentLD.value = arrayListOf(
-            Student(
-                "16055",
-                "Nonie",
-                "1998/03/28",
-                "5718444778",
-                "https://i.pinimg.com/originals/62/8c/01/628c01e4b8321396e45d812a871ffd26.jpg"
-            ),
-            Student(
-                "13312",
-                "Rich",
-                "1994/12/14",
-                "3925444073",
-                "https://th.bing.com/th/id/OIP.nBBABfTYuzj2DrsaqZ7pJgAAAA?rs=1&pid=ImgDetMain"
-            ),
-            Student(
-                "11204",
-                "Dinny",
-                "1994/10/07",
-                "6827808747",
-                "https://i.pinimg.com/originals/e8/33/43/e83343f654a670939e13fb38b15782e6.jpg"
-            )
-        )
+//        studentLD.value = arrayListOf(
+//            Student(
+//                "16055",
+//                "Nonie",
+//                "1998/03/28",
+//                "5718444778",
+//                "https://i.pinimg.com/originals/62/8c/01/628c01e4b8321396e45d812a871ffd26.jpg"
+//            ),
+//            Student(
+//                "13312",
+//                "Rich",
+//                "1994/12/14",
+//                "3925444073",
+//                "https://th.bing.com/th/id/OIP.nBBABfTYuzj2DrsaqZ7pJgAAAA?rs=1&pid=ImgDetMain"
+//            ),
+//            Student(
+//                "11204",
+//                "Dinny",
+//                "1994/10/07",
+//                "6827808747",
+//                "https://i.pinimg.com/originals/e8/33/43/e83343f654a670939e13fb38b15782e6.jpg"
+//            )
+//        )
         studentLoadErrorLD.value = false
         LoadingLD.value = true
 
@@ -54,10 +54,12 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             {
-
+                val sType = object : TypeToken<ArrayList<Student>>() {}.type
+                val result = Gson().fromJson<ArrayList<Student>>(it, sType)
+                studentLD.value = result
 //                if success, set loading progress live data to false
                 LoadingLD.value = false
-                Log.d("showvolley", it)
+                Log.d("showvolley", result.toString())
             },
             {
 //                if failed, show error message, set error live data to true and progress live data to false
@@ -65,6 +67,16 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
                 studentLoadErrorLD.value = true
                 LoadingLD.value = false
             }
-        )
+        ).apply {
+//            this.tag = TAG
+            tag = TAG
+            queue?.add(this)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        queue?.cancelAll(TAG)
     }
 }

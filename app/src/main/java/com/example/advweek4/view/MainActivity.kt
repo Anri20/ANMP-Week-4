a1bb4a4a7c540ad4b7949520d5249b1f46c90559
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-03-04 09:28:02 +0700

    Create showNotif Function

--- app/src/main/java/com/example/advweek4/view/MainActivity.kt
+++ app/src/main/java/com/example/advweek4/view/MainActivity.kt
@@ -1,12 +1,109 @@
 package com.example.advweek4.view
 
+import android.Manifest
+import android.app.NotificationManager
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.advweek4.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MainActivity? = null
+
+        fun showNotification(title: String, content: String, icon: Int){
+            val channelId = "${instance?.packageName}-${instance?.getString(R.string.app_name)}"
+            val notificationBuilder = NotificationCompat.Builder(instance!!.applicationContext, channelId).apply {
+                setSmallIcon(icon)
+                setContentTitle(title)
+                setContentText(content)
+                setStyle(NotificationCompat.BigTextStyle())
+                priority = NotificationCompat.PRIORITY_DEFAULT
+                setAutoCancel(true)
+            }
+
+            val notificationManager = NotificationManagerCompat.from(instance!!.applicationContext.applicationContext!!)
+
+            if (ActivityCompat.checkSelfPermission(
+                    instance!!,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notificationManager.notify(1001, notificationBuilder.build())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val observable = Observable.just("a stream of data", "Hello", "RxJava")
+
+        val observer = object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d("RxJava", "begin subscribe")
+            }
+
+            override fun onNext(t: String) {
+                Log.d("RxJava", "data: $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("RxError", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("RxJava", "complete")
+            }
+        }
+
+        observable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer)
+
+        Observable.just("Another stream of data", "Hello again", "RxKotlin")
+            .subscribe(
+                {
+                    Log.d("RxKotlin", it)
+                },
+                {
+                    Log.d("RxError", it.message.toString())
+                },
+                {
+                    Log.d("RxKotlin", "Finish!")
+                }
+            )
+
+        Observable.timer(5, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("delay", "delay of five seconds")
+            })
     }
 }
